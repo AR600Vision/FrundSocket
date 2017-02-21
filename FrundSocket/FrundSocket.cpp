82db78dbe2de20d35e36328a186dccbb4c045d17
Markov Alex <markovalex95@gmail.com>
2017-02-22 01:33:20 +0300

    Пофиксил ошибки - при инициализации сокетов я передвал всегда адрес и порт контроля.

--- FrundSocket/FrundSocket.cpp
+++ FrundSocket/FrundSocket.cpp
@@ -6,9 +6,9 @@
 
 int main()
 {
-	double* xin;
-	double* xout;
-	double* par;
+	double* xin = new double[1000];
+	double* xout = new double[1000];
+	double par[] = { 0, 1, 2, 3, 4 };
 
 	gcontrol(1, xin, xout, par);
 
--- FrundSocket/frund.cpp
+++ FrundSocket/frund.cpp
@@ -4,12 +4,16 @@
 #pragma comment(lib,"ws2_32.lib")	//Winsock Library
 
 #define KDRIVE 21
-#define BUFLEN 300					//KDRIVE*9  //Max length of buffer
+#define IN_BUFLEN 300					//KDRIVE*9  //Max length of buffer
+#define OUT_BUFLEN 100
 
-#define SERVER "127.0.0.1"			//ip address of udp server
-#define PORT 55556					//The port on which to listen for incoming data
-#define ROS_IP "127.0.0.1"
-#define ROS_PORT 123
+//Парааметры подключения к программе управления
+#define CONTROL_IP "127.0.0.1"
+#define CONTROL_PORT 55556
+
+//Параметры подключения к ROS
+#define ROS_IP "192.168.1.104"
+#define ROS_PORT 12833
 
 //int slen;
 bool isFirst = true;
@@ -26,9 +30,7 @@ void initSocket(int port, const char* ip, SOCKET & sock, struct sockaddr_in& add
 void control_request(double t, double* xin, double* xout);
 void ros_request(double t,  double* par);
 
-//Вот эта функция, которую вызывает горобцов
-//Данные будут переданы и возвращены через par
-//Еще он как-то будет указывать, что хочет вызвать - пашину или нашу
+//Отправка данных
 /*extern "C"*/ void gcontrol(double t, double* xin, double* xout, double* par)
 {
 	if (isFirst)
@@ -38,8 +40,11 @@ void ros_request(double t,  double* par);
 		isFirst = false;
 	}
 
-	//Вызываем либо Пашину go, либо свою
-	control_request(t, xin, xout);
+	//Отправка управления
+	//control_request(t, xin, xout);
+
+	//Отправка в ROS
+	ros_request(t, par);
 }
 
 void initSockets()
@@ -54,7 +59,7 @@ void initSockets()
 	printf("Initialised.\n");
 
 	//create socket
-	initSocket(PORT, SERVER, socket_control, si_control);
+	initSocket(CONTROL_PORT, CONTROL_IP, socket_control, si_control);
 	initSocket(ROS_PORT, ROS_IP, socket_ros, si_ros);
 }
 
@@ -69,8 +74,8 @@ void initSocket(int port, const char* ip, SOCKET & sock, struct sockaddr_in & ad
 
 	memset((char *)&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(PORT);
-	addr.sin_addr.S_un.S_addr = inet_addr(SERVER);
+	addr.sin_port = htons(port);
+	addr.sin_addr.S_un.S_addr = inet_addr(ip);
 }
 
 //Отправка данных системе управления
@@ -78,13 +83,13 @@ void control_request(double t, double* xin, double* xout)
 {
 
 	int slen = sizeof(si_control);
-	if (sendto(socket_control, (char*)xin, BUFLEN * sizeof(double), 0, (struct sockaddr *) &si_control, slen) == SOCKET_ERROR)
+	if (sendto(socket_control, (char*)xin, IN_BUFLEN * sizeof(double), 0, (struct sockaddr *) &si_control, slen) == SOCKET_ERROR)
 	{
 		printf("sendto() failed with error code : %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
-	if (recvfrom(socket_control, (char*)xout, 100 * sizeof(double), 0, (struct sockaddr *) &si_control, &slen) == SOCKET_ERROR)
+	if (recvfrom(socket_control, (char*)xout, OUT_BUFLEN * sizeof(double), 0, (struct sockaddr *) &si_control, &slen) == SOCKET_ERROR)
 	{
 		printf("recvfrom() failed with error code : %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
@@ -92,26 +97,55 @@ void control_request(double t, double* xin, double* xout)
 	return;
 }
 
-//Отправка данных ROS
+/* ОТПРАВКА КОМАНД В ROS
+
+-------------------- ФОРМАТ ДАННЫХ --------------------
+*  данные пеередаются в виде массива double
+*
+*  Запрос:
+*  |---------|------|------ ... ---|
+*  | node id | rviz | data         |
+*  |---------|------|------ ... ---|
+*
+*  node id      номер вызываемой ноды
+*  rviz         входные данные беруться из запроса (0) или из RVIZ (1) (или еще откуда-нибудь)
+*
+*  Ответ:
+*  |---------|--------|--------|--------------|------ ... ---|
+*  | node id | status | actual | elapsed time | data         |
+*  |---------|--------|--------|--------------|------ ... ---|
+*
+*  node id       Номер ноды, которая отвечает (такой же, как в запросе
+*  status        Код ошибки
+*  actual        Является ли эти данные ответом на текущий (1) или один из предыдущих (0)
+*                запросов. В этой версии ответ в пределах одного запроса невозможен, всегда 0
+*  elapsed time  Время, прошедшее с момента запроса. В этой версии всегда 0.
+*
+*  КОДЫ ОШИБОК:
+*   0     Без ошибок
+*   1     Неверный формат пакета
+*   2     Запрошенная нода не найдена
+*   3     Неизвестная внутренняя ошибка в ноде
+*/
 void ros_request(double t, double * par)
 {
-	/*
-	1. Определять, какую ноду - ???
-	2. Размер буфера - ???
-	*/
+	//Определение размера передаваемых и получаемых данных
+	int sendDataSize;		//Размер передаваемых данных	
+	int recvCount;			//Размер реально полученного массива
 
-	int inLen = 1;
+	//Параметр sendDataSize зависит от типа вызываемой ноды
+	//Что делать с recvCount???
 
-	int slen = sizeof(si_control);
-	if (sendto(socket_control, (char*)par, inLen * sizeof(double), 0, (struct sockaddr *) &si_control, slen) == SOCKET_ERROR)
+	sendDataSize = 5;
+
+	int slen = sizeof(si_ros);
+	if (sendto(socket_ros, (char*)par, sendDataSize * sizeof(double), 0, (struct sockaddr *) &si_ros, slen) == SOCKET_ERROR)
 	{
 		printf("sendto() failed with error code : %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 
-	int outLen = 1;
-
-	if (recvfrom(socket_control, (char*)par, outLen * sizeof(double), 0, (struct sockaddr *) &si_control, &slen) == SOCKET_ERROR)
+	if ((recvCount = recvfrom(socket_ros, (char*)par, OUT_BUFLEN * sizeof(double), 0, (struct sockaddr *) &si_ros, &slen)) == SOCKET_ERROR)
 	{
 		printf("recvfrom() failed with error code : %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
